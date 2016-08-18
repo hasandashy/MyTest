9b55584ef322455f4a860c790f4cb801d6d8c558
hasandashy <mohdhasan.27june@gmail.com>
2016-08-18 13:05:11 +0530

    commit

---
+++ ServiceTest/ServiceTest/WebForm1.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using ServiceTest.AuthenticationRef;
+
+namespace ServiceTest
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            AuthenticationWebServiceSoapClient _asc = new AuthenticationWebServiceSoapClient();
+            //_asc.
+
+            string strTest = _asc.Login("17F742F6-1ACF-452A-A114-55116BA1CAB3", "lliu@hrpa.ca", "123456");
+            
+        }
+    }
+}
